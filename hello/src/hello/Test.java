c2f8257bd285037d145ed68c73c9daa34fba5e09
User <User@DESKTOP-L5H9MLI>
2021-06-11 21:17:49 +0530

    add method added

--- hello/src/hello/Test.java
+++ hello/src/hello/Test.java
@@ -1,11 +1,15 @@
 package hello;
 
 public class Test {
-
+	
+	public static int add(int a,int b) {
+		return a+b;
+	}
 	public static void main(String[] args) {
 		System.out.println("Main method added and file is pushed from eclipse");
 		Employee e = new Employee(114, "Shubham");
 		System.out.println(e.getName());
+		System.out.println(add(10, 20));
 	}
 
 }
