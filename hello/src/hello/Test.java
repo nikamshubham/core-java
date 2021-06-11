b70fea0a7a270888145b7b88a25786c9c8f64db6
User <User@DESKTOP-L5H9MLI>
2021-06-11 21:02:25 +0530

    Employee class added

--- hello/src/hello/Test.java
+++ hello/src/hello/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Main method added and file is pushed from eclipse");
+		Employee e = new Employee(114, "Shubham");
+		System.out.println(e.getName());
 	}
 
 }
