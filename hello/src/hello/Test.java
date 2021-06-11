62c13612c9d0e923df6316a591f678585a000fd5
User <User@DESKTOP-L5H9MLI>
2021-06-11 21:20:06 +0530

    substract method added in dev

--- hello/src/hello/Test.java
+++ hello/src/hello/Test.java
@@ -5,11 +5,15 @@ public class Test {
 	public static int add(int a,int b) {
 		return a+b;
 	}
+	public static int substract(int a,int b) {
+		return a-b;
+	}
 	public static void main(String[] args) {
 		System.out.println("Main method added and file is pushed from eclipse");
 		Employee e = new Employee(114, "Shubham");
 		System.out.println(e.getName());
 		System.out.println(add(10, 20));
+		System.out.println(substract(20, 10));
 	}
 
 }
